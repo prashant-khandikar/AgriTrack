ae542d7a90ccfe6b6a06f6e10eeb799473e083d4
Prashant Khandikar <prashantkhandikar@gmail.com>
2026-04-26 23:27:01 +0530

    Initial project setup

---
+++ src/main/java/symbiosisProject/AgriTrack/controller/HealthCheck.java
@@ -0,0 +1,16 @@
+package symbiosisProject.AgriTrack.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/check")
+public class HealthCheck {
+
+	@GetMapping
+	public String check()
+	{
+		return"all ok";
+	}
+}
