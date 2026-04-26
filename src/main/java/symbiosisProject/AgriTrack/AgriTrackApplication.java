84c5797406895bfae9d35e985725dfeba7b1d181
Prashant Khandikar <prashantkhandikar@gmail.com>
2026-04-26 20:15:03 +0530

    Initial Commit

---
+++ src/main/java/symbiosisProject/AgriTrack/AgriTrackApplication.java
@@ -0,0 +1,13 @@
+package symbiosisProject.AgriTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/symbiosisProject/AgriTrack/AgriTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package symbiosisProject.AgriTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
